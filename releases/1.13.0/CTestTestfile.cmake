935044683dda561bf4f540d7d3056d29a2d993a7
Bharath Ramaswamy <quic_bharathr@quicinc.com>
2020-12-03 19:36:04 +0000

    Added documentation for release 1.13.0
    Signed-off-by: Bharath Ramaswamy <quic_bharathr@quicinc.com>

---
+++ releases/1.13.0/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ubuntu/workspace/AIMET_PreBuilt_Release/aimet/Docs
+# Build directory: /home/ubuntu/workspace/AIMET_PreBuilt_Release/aimet/build/Docs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
