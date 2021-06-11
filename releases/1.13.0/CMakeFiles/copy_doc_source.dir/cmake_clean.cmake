94019648232ee0b3d17e181ca43ddbf6d0f08203
Bharath Ramaswamy <quic_bharathr@quicinc.com>
2021-06-10 19:19:33 -0700

    Added documentation for 1.16.1 python 3.7 (code colab), Removed all cmake and make files
    Signed-off-by: Bharath Ramaswamy <quic_bharathr@quicinc.com>

--- releases/1.13.0/CMakeFiles/copy_doc_source.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/copy_doc_source"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/copy_doc_source.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
