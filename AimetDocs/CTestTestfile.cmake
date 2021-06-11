22538e60e8edc3992ba3ebdfa8acf83f0ab4adec
Bharath Ramaswamy <quic_bharathr@quicinc.com>
2021-06-10 19:19:33 -0700

    Added documentation for 1.16.1 python 3.7 (code colab), Removed all cmake and make files
    Signed-off-by: Bharath Ramaswamy <quic_bharathr@quicinc.com>

--- AimetDocs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /local/mnt/workspace/jenkins-slave/workspace/AIMET_MIRROR-CD-PIPELINE/mltt/Docs
-# Build directory: /local/mnt/workspace/jenkins-slave/workspace/AIMET_MIRROR-CD-PIPELINE/build/Docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- releases/1.16.1/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubuntu/workspace/AIMET_PreBuilt_Release/tf-torch-cpu/aimet/Docs
-# Build directory: /home/ubuntu/workspace/AIMET_PreBuilt_Release/tf-torch-cpu/aimet/build/Docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
